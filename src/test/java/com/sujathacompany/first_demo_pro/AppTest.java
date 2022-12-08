dbc56b4816423eb5875c6f6f2b7d0fcc229113af
sujatha238 <sujatha.racharla@gmail.com>
2022-12-08 14:17:20 +0530

    myinital commit maven

---
+++ src/test/java/com/sujathacompany/first_demo_pro/AppTest.java
@@ -0,0 +1,20 @@
+package com.sujathacompany.first_demo_pro;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
